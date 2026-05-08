990fd4035802904e11954d4018f9836d78a1b92f
duwupitona8let <l-kolmakov@mai.ru>
2026-05-08 07:54:56 +0000

    arhitektura fix

--- ci/package.sh
+++ ci/package.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 set -e
 
-# Сборка deb-пакета
 echo "Сборка deb-пакета..."
 
-# Создание структуры директорий для пакета
+# Определяем архитектуру
+ARCH=$(dpkg --print-architecture)
+echo "Архитектура: $ARCH"
+
+# Создаём структуру
 mkdir -p processor_1.0/usr/local/bin
 mkdir -p processor_1.0/DEBIAN
 
-# Копирование бинарного файла в пакет
+# Копируем бинарник
 cp processor processor_1.0/usr/local/bin/
 
-# Создание файла control с метаданными пакета
-cat > processor_1.0/DEBIAN/control << 'EOC'
+# Создаём control файл с правильной архитектурой
+cat > processor_1.0/DEBIAN/control << EOC
 Package: processor
 Version: 1.0
 Section: utils
 Priority: optional
-Architecture: arm64
+Architecture: $ARCH
 Depends: libc6
 Maintainer: Student <student@example.com>
 Description: String processor - replaces spaces with most frequent character
 EOC
 
-# Сборка deb-пакета
+# Собираем пакет
 dpkg-deb --build processor_1.0
 
-echo "Пакет создан: processor_1.0.deb"
+echo "Пакет создан: processor_1.0.deb ($ARCH)"
